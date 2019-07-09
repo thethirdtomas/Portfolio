1c10513839959334c975d5653568b40d4c6fc9c9
Tomas Torres <tomas3torres@live.com>
2019-07-09 18:42:03 -0500

    fix mail bug

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,9 +14,11 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "tomas3torres@live.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@tomas3torrescom\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: noreply@tomas3torres.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body))
+if(!mail($to, $subject, $body, $header))
   http_response_code(500);
+
+mail($to, $subject, $body, $header)
 ?>
